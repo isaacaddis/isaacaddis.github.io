4a42cee01ccd6ddc958c22a39ae919a44362eaac
Isaac Addis <isaacaddis21@gmail.com>
2016-07-04 13:45:40 -0700

    Index
    Beginning of Mobile Optimization

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,8 @@
     $(document).ready(function(){
-      $(".button-collapse").sideNav();
+	$('.button-collapse').sideNav({
+	      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+	    }
+	  );
+  $('.collapsible').collapsible();
       $('.slider').slider({full_width:true});
     });
