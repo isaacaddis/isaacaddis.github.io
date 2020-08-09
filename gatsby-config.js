7f5b69e33b853f271db8fb616b1a9b7210a680b0
Isaac Addis <isaacaddis21@gmail.com>
2020-08-09 14:01:18 -0700

    typescript rewrite

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-feed-mdx`,
+    `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
